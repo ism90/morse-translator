905ea9d630ecbb43404d5c0f225d54b0e30f985c
ism90 <ianmason5@gmail.com>
2021-09-17 15:31:02 +0100

    Basic funtion to test for valid input

--- scripts/translator.js
+++ scripts/translator.js
@@ -1,2 +1,16 @@
 import { alphabet } from "./data";
 
+export const translator = (input) => {
+  if (typeof input === "string" && input.length > 0) {
+    let inputToLowerCase = input.toLowerCase()
+  
+  
+  
+  
+  
+  
+  } else {
+    return "Not a valid input"
+  }
+
+}
