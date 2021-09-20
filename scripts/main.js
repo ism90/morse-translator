65405c438390fd6bfc5193d97ab7f714533b394b
ism90 <ianmason5@gmail.com>
2021-09-20 23:50:53 +0100

    Styling, responsive, sound added

--- scripts/main.js
+++ scripts/main.js
@@ -7,25 +7,36 @@ const input = document.querySelector(".input__box");
 const output = document.querySelector(".output__area");
 const button = document.querySelector(".button");
 const clear = document.querySelector(".clear");
+const sound = document.querySelector(".sound");
 
 const determineTranslation = () => {
   let getTranslateType = input.value;
   //RegEx search pattern for Morse input
-  if  (/^[.\/ -]*$/gm.test(getTranslateType)) {
+  if (/^[.\/ -]*$/gm.test(getTranslateType)) {
     // If Morse input
-    const translationToEnglish = translator.translatorForEnglish(getTranslateType);
+    const translationToEnglish =
+      translator.translatorForEnglish(getTranslateType);
     output.innerHTML = translationToEnglish;
   } else {
-    const translationToMorse =
-      translator.translatorForMorse(getTranslateType);
+    const translationToMorse = translator.translatorForMorse(getTranslateType);
     output.innerHTML = translationToMorse;
   }
 };
+
+// Event Listeners for Buttons
+
 button.addEventListener("click", () => {
   determineTranslation();
 });
 
 clear.addEventListener("click", () => {
   input.value = "";
+  output.innerHTML = "";
 });
 
+// Audio
+
+sound.addEventListener("click", () => {
+  let morseSound = new morseSynth();
+  morseSound.play(input.value);
+});
